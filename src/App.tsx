006a4af273188d52be0ce831e7a616ea53f18495
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-06 22:35:14 +0000

    Add videos

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 import Header from './components/header'
 import './scss/App.scss'
+import './videos/coin.mp4'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <main>
-
+        <video>
+          <source type="video/mp4" src="./videos/coin.mp4" />
+        </video>
       </main>
     </div>
   );
---
+++ src/components/video.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './scss/App.scss'
+
+const Video = () => {
+  return (
+    <div className="App">
+      <section>
+
+      </section>
+    </div>
+  );
+}
+
+export default Video
