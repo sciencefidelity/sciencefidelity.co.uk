c3c62b2baafd1a277d315a470788b07b23aac342
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-17 21:03:52 +0000

    Add a page for the videos

--- svelte/src/routes/dance.svelte
+++ svelte/src/routes/dance.svelte
@@ -7,13 +7,12 @@
   <meta name="description" content="Scroll to make the objects dance.">
 </svelte:head>
 
-<div id="nav">
-  <a sveltekit:prefetch href="./dance"><div>🏓</div></a>
-  <a sveltekit:prefetch href="./dance"><div>🪙</div></a>
-  <a sveltekit:prefetch href="./dance"><div>🔥</div></a>
-  <a sveltekit:prefetch href="./dance"><div>✂️</div></a>
-  <a sveltekit:prefetch href="./dance"><div>🙂</div></a>
-</div>
+<a href="/">
+  <div id="home">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 5.69l5 4.5V18h-2v-6H9v6H7v-7.81l5-4.5M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3z"/></svg>
+  </div>
+</a>
+<canvas id="anim" data-id="ball" data-count="830"/>
 
 <style lang="scss">
 * {
