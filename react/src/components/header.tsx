def9c4724f1c281c2f199605b0ad4f1d08fdf689
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-18 21:44:20 +0000

    Create an array of data values

--- react/src/components/header.tsx
+++ react/src/components/header.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import '../scss/header.scss'
 
 const Header = () => {
+
+  const dataId:string[] = ['ball', 'coin', 'flame', 'scissors', 'bounce']
+  const dataCount:number[] = [830, 602, 600, 337, 260]
+
   return (
     <header>
       <nav>
@@ -15,7 +19,7 @@ const Header = () => {
         </ul>
       </nav>
     </header>
-  );
+  )
 }
 
 export default Header
