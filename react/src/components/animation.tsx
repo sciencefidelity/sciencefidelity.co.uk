a999e5a9d939cd6a5d899a284bab399a32e6dbb2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-06-18 14:53:39 +0000

    Get the scroll based video working

--- react/src/components/animation.tsx
+++ react/src/components/animation.tsx
@@ -1,59 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import '../scss/video.scss'
+import '../scss/animation.scss'
 
 const Animation = () => {
 
-  const html = document.documentElement
-  const canvas = document.getElementById('anim') as HTMLCanvasElement
-  const context = canvas.getContext('2d')
+  useEffect(() => {
+    const html = document.documentElement
+    const canvas = document.getElementById('anim') as HTMLCanvasElement
+    const context = canvas.getContext('2d')
 
-  const frameCount = Number(canvas.dataset.count)
-  const currentFrame = (index:number) => (
-    `../videos/${canvas.dataset.id}/frame-${index.toString().padStart(6, '0')}.TIFF.webp`
-  )
-
-  document.body.style.height = `${frameCount * 3}vh`
-
-  const preloadImages = () => {
-    for (let i = 1; i < frameCount; i++) {
-      const img = new Image()
-      img.src = currentFrame(i)
-    }
-  }
-
-  const img = new Image()
-  img.src = currentFrame(1)
-  canvas.width=1920
-  canvas.height=1080
-  img.onload=function(){
-    context!.drawImage(img, 0, 0)
-  }
-
-  const updateImage = (index:number) => {
-    img.src = currentFrame(index)
-    context!.drawImage(img, 0, 0)
-  }
-
-  window.addEventListener('scroll', () => {
-    const scrollTop = html.scrollTop
-    const maxScrollTop = html.scrollHeight - window.innerHeight
-    const scrollFraction = scrollTop / maxScrollTop
-    const frameIndex = Math.min(
-      frameCount - 1,
-      Math.ceil(scrollFraction * frameCount)
+    const frameCount = Number(canvas.dataset.count)
+    const currentFrame = (index:number) => (
+      `../videos/${canvas.dataset.id}/frame-${index.toString().padStart(6, '0')}.TIFF.webp`
     )
 
-    requestAnimationFrame(() => updateImage(frameIndex + 1))
-  })
+    document.body.style.height = `${frameCount * 3}vh`
 
-  window.onload = preloadImages
+    const preloadImages = () => {
+      for (let i = 1; i < frameCount; i++) {
+        const img = new Image()
+        img.src = currentFrame(i)
+      }
+    }
+
+    const img = new Image()
+    img.src = currentFrame(1)
+    canvas.width=1920
+    canvas.height=1080
+    img.onload=function(){
+      context!.drawImage(img, 0, 0)
+    }
+
+    const updateImage = (index:number) => {
+      img.src = currentFrame(index)
+      context!.drawImage(img, 0, 0)
+    }
+
+    window.addEventListener('scroll', () => {
+      const scrollTop = html.scrollTop
+      const maxScrollTop = html.scrollHeight - window.innerHeight
+      const scrollFraction = scrollTop / maxScrollTop
+      const frameIndex = Math.min(
+        frameCount - 1,
+        Math.ceil(scrollFraction * frameCount)
+      )
+
+      requestAnimationFrame(() => updateImage(frameIndex + 1))
+    })
+
+    preloadImages()
+  })
 
   return (
     <section>
-      <canvas id="anim" data-id="ball" data-count="830"/>
+      <canvas
+        id="anim"
+        data-id="ball"
+        data-count="830"
+        height="1080"
+        width="1920"
+      />
     </section>
-  );
+  )
 }
 
 export default Animation
