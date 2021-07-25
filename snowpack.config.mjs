ad602ebc8e295a4a6add07a72621be519a7030ed
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-07-25 14:10:50 +0000

    Set up ESLint

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -11,6 +11,14 @@ export default {
     '@snowpack/plugin-sass',
     '@snowpack/plugin-typescript',
     '@snowpack/plugin-webpack',
+    [
+      '@snowpack/plugin-run-script',
+      {
+        cmd: 'eslint src --ext .js,.jsx,.ts,.tsx',
+        // Optional: Use npm package "eslint-watch" to run on every file change
+        watch: 'esw -w --clear src --ext .js,.jsx,.ts,.tsx',
+      },
+    ],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
