811166a00c3dc70864dab39a3f883e52e8ca422d
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-17 23:47:20 +0100

    feat: render the default image from imagekit

--- web/src/pages/Index.tsx
+++ web/src/pages/Index.tsx
@@ -1,8 +1,16 @@
 import React, { FC } from "react"
+import { IKImage } from "imagekitio-react"
+
+const urlEndpoint = import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT
 
 const Index: FC = () => {
   return (
-    <div className="container">Hello world!</div>
+    <div>
+      <IKImage
+        urlEndpoint={urlEndpoint}
+        path="default-image.jpg"
+      />
+    </div>
   )
 }
 export default Index
