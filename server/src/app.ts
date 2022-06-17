269ffdfcc5e895d5055f4c82fd75c682ac58120c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-17 23:18:16 +0100

    feat: set up auth server for imagekit uploads

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,28 +1,29 @@
-// const express = require('express');
-// const app = express();
-// const ImageKit = require('imagekit');
-//
-// const imagekit = new ImageKit({
-//   urlEndpoint: '<YOUR_IMAGEKIT_URL_ENDPOINT>',
-//   publicKey: '<YOUR_IMAGEKIT_PUBLIC_KEY>',
-//   privateKey: '<YOUR_IMAGEKIT_PRIVATE_KEY>'
-// });
-//
-// // allow cross-origin requests
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-//
-// app.get('/auth', function (req, res) {
-//   var result = imagekit.getAuthenticationParameters();
-//   res.send(result);
-// });
-//
-// app.listen(3001, function () {
-//   console.log('Live at Port 3001');
-// });
+import * as dotenv from "dotenv"
+import "module-alias/register"
+import express from "express"
+import ImageKit from "imagekit"
 
-console.log("hello")
+dotenv.config({ path: ".env" })
+
+const app = express()
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || "",
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY || "",
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY || ""
+})
+
+app.use(function(_req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+app.get("/auth", function (_req, res) {
+  var result = imagekit.getAuthenticationParameters()
+  res.send(result)
+});
+
+app.listen(3001, function () {
+  console.log("Live at Port 3001")
+})
