b70d6af1e21c42190522e28d336f0ffd4ca47262
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-20 22:10:06 +0100

    feat: render a map

--- mapbox/src/App.tsx
+++ mapbox/src/App.tsx
@@ -1,6 +1,28 @@
-import { FC } from "react"
+import { useRef, useEffect, useState } from "react"
+import mapboxgl from "mapbox-gl"
 
-const App: FC = () => {
-  return <div>Hello world!</div>
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
+
+const App = () => {
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+
+  useEffect(() => {
+    if (map.current) return
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [lng, lat],
+      zoom: zoom
+    })
+  })
+  return (
+    <div>
+      <div ref={mapContainer} className="mapContainer" />
+    </div>
+  )
 }
 export default App
