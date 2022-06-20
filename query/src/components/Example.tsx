551cbcbe62d7ae822928d0232f1c90f534c81ef4
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-20 03:16:24 +0100

    refactor: experimant with react-query vs fetch

--- query/src/components/Example.tsx
+++ query/src/components/Example.tsx
@@ -1,39 +1,38 @@
 import { useQuery } from "react-query"
 
+const req = new Request(
+  "https://api.github.com/repos/sciencefidelity/Nova-Astro"
+)
+// async function getGithubStats() {
+//   try {
+//     const res = await fetch(req)
+//     const data = await res.json()
+//     return data
+//   } catch (error) {
+//     console.log("ERROR", error.message)
+//   }
+// }
+// const data = await getGithubStats()
+
 export const Example = () => {
-  // const { isLoading, error, data } = useQuery("repoData", () =>
-  //   fetch("https://api.github.com/repos/sciencefidelity/Nova-Astro").then(res =>
-  //     res.json()
-  //   )
-  // )
-
-  // if (isLoading) return <div>Loading...</div>
-  // let errorMessage = "An unknown error has occurred"
-  // if (error instanceof Error) {
-  //   errorMessage = `An error has occurred: ${error.message}`
-  // }
-  // if (error) return <div>{errorMessage}</div>
-
-  const myRequest = new Request(
-    "https://api.github.com/repos/sciencefidelity/Nova-Astro"
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch(req).then(res => res.json())
   )
 
-  const getGithubData = async () => {
-    await fetch(myRequest).then()
-      .then(res => res.json())
-      .then(data => data)
+  if (isLoading) return <div>Loading...</div>
+  let errorMessage = "An unknown error has occurred"
+  if (error instanceof Error) {
+    errorMessage = `An error has occurred: ${error.message}`
   }
-  const data = getGithubData()
-  console.log(data)
+  if (error) return <div>{errorMessage}</div>
+
   return (
     <div>
-      <pre>{JSON.stringify(data, undefined, 2)}</pre>
-      {/* <h1>{data.name}</h1>
+      <h1>{data.name}</h1>
       <p>{data.description}</p>
       <strong>👀 {data.subscribers_count}</strong>{" "}
       <strong>✨ {data.stargazers_count}</strong>{" "}
       <strong>🍴 {data.forks_count}</strong>
-      <p>Client is loaded</p> */}
     </div>
   )
 }
