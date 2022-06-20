e525a8c54fa3a9e601d3cda400cf339b2fbc9295
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-20 01:29:21 +0100

    refactor: experiment with react-query and fetch

--- query/src/components/Example.tsx
+++ query/src/components/Example.tsx
@@ -1,19 +1,39 @@
 import { useQuery } from "react-query"
 
 export const Example = () => {
-  const { isLoading, error, data } = useQuery("repoData", () =>
-    fetch("https://api.github.com/repos/sciencefidelity/Nova-Astro").then(res =>
-      res.json
-    )
+  // const { isLoading, error, data } = useQuery("repoData", () =>
+  //   fetch("https://api.github.com/repos/sciencefidelity/Nova-Astro").then(res =>
+  //     res.json()
+  //   )
+  // )
+
+  // if (isLoading) return <div>Loading...</div>
+  // let errorMessage = "An unknown error has occurred"
+  // if (error instanceof Error) {
+  //   errorMessage = `An error has occurred: ${error.message}`
+  // }
+  // if (error) return <div>{errorMessage}</div>
+
+  const myRequest = new Request(
+    "https://api.github.com/repos/sciencefidelity/Nova-Astro"
   )
 
-  if (isLoading) return "Loading..."
-
-  if (error) return "An error has occurred: " + error
-
+  const getGithubData = async () => {
+    await fetch(myRequest).then()
+      .then(res => res.json())
+      .then(data => data)
+  }
+  const data = getGithubData()
+  console.log(data)
   return (
     <div>
-      {data?.name && <h1>{data.name}</h1>}
+      <pre>{JSON.stringify(data, undefined, 2)}</pre>
+      {/* <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
+      <p>Client is loaded</p> */}
     </div>
   )
 }
