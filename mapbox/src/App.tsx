059da0cda7c6d6d76c582da125ba94edd8f98da1
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-20 22:22:36 +0100

    refactor: update coordinates and zoom in state

--- mapbox/src/App.tsx
+++ mapbox/src/App.tsx
@@ -1,11 +1,11 @@
 import { useRef, useEffect, useState } from "react"
-import mapboxgl from "mapbox-gl"
+import mapboxgl, { Map } from "mapbox-gl"
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
 
 const App = () => {
-  const mapContainer = useRef(null)
-  const map = useRef(null)
+  const mapContainer = useRef<any>(null)
+  const map = useRef<any>(null)
   const [lng, setLng] = useState(-70.9)
   const [lat, setLat] = useState(42.35)
   const [zoom, setZoom] = useState(9)
@@ -17,6 +17,15 @@ const App = () => {
       style: "mapbox://styles/mapbox/streets-v11",
       center: [lng, lat],
       zoom: zoom
+    })
+  })
+
+  useEffect(() => {
+    if (!map.current) return
+    map.current.on("move", () => {
+      setLng(map?.current?.getCenter().lng.toFixed(4))
+      setLat(map?.current?.getCenter().lat.toFixed(4))
+      setZoom(map?.current?.getZoom().toFixed(2))
     })
   })
   return (
