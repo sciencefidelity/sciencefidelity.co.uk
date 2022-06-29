1b4e008f818dfcaa9666bc5a09c4fb4356e6550e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-29 00:20:26 +0000

    refactor: add plugin to connect with mongodb atlas

---
+++ artmap/src/dbConnector.ts
@@ -0,0 +1,12 @@
+import fastfyPlugin, { FastifyPlugin } from "fastify"
+import fastifyMongodb from "@fastify/mongodb"
+import { FastifyInstance } from "fastify"
+
+
+async function dbConnector(fastify: FastifyInstance, _options: unknown) {
+  fastify.register(fastifyMongodb, {
+    url: process.env.MONGO_CONNECTION_STRING
+  }) 
+}
+
+module.exports = fastfyPlugin(dbConnector)
